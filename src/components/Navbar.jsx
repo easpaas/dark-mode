0a7587d9148a48399811cc61af1f57523bba3f12
evan <aspaasevan@gmail.com>
2020-04-08 15:24:35 -0700

    MVP complete

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,10 @@
-import React, { useState } from 'react';
-import { useLocalStorage } from '../hooks/useLocalStorage';
+import React from 'react';
+import { useDarkMode } from '../hooks/useDarkMode';
 
 
 const Navbar = () => {
-  const [darkMode, setDarkMode] = useState(false);
+  const [darkMode, setDarkMode] = useDarkMode(false);
+  
   const toggleMode = e => {
     e.preventDefault();
     setDarkMode(!darkMode);
--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -1,12 +1,12 @@
-import {useState, useEffect} from 'react';
+import {useEffect} from 'react';
 import {useLocalStorage} from './useLocalStorage';
 
 export const useDarkMode = key => {
   const [someValue, setSomeValue] = useLocalStorage(key);
 
   useEffect(() => {
-    someValue ? console.log(key) : console.log(someValue)
-  }, []);
+    someValue ? document.body.classList.add('dark-mode') : document.body.classList.remove('dark-mode')
+  }, [someValue]);
 
   return [someValue, setSomeValue];
 }
